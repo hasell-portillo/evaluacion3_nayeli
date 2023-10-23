a23fee366d510471f29107bf6e878c1a118fd339
hasell-portillo <139635946+hasell-portillo@users.noreply.github.com>
2023-10-22 19:47:58 -0600

    agregando lo draggel

--- lib/draggablescrollablesheet.dart
+++ lib/draggablescrollablesheet.dart
@@ -1,15 +1,59 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
 class ConfiguracionPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Configuración'),
+        title: Text('draggablescrollablesheet'),
       ),
       body: Center(
-        child: Text('Página de configuración'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Página de draggablescrollablesheet'),
+            ElevatedButton(
+              onPressed: () {
+                showModalBottomSheet(
+                  context: context,
+                  builder: (context) {
+                    return Container(
+                      color: Colors.grey[200],
+                      child: ListView(
+                        children: <Widget>[
+                          ListTile(
+                            title: Text('bob esponja pantalones cuadrados'),
+                          ),
+                          ListTile(
+                            title: Text('bts'),
+                          ),
+                          ListTile(
+                            title: Text('pokemones'),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                );
+              },
+              child: Text('Mostrar Hoja Desplegable'),
+            ),
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ConfiguracionPage(),
     );
   }
 }
