486c9387ba1b2274731018bf342f7e07c7f1627f
hasell-portillo <139635946+hasell-portillo@users.noreply.github.com>
2023-10-22 19:15:00 -0600

    creando apartado single

---
+++ lib/singlechildscrollview.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AcercaDePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Acerca de'),
+      ),
+      body: Center(
+        child: Text('Información sobre la aplicación'),
+      ),
+    );
+  }
+}
