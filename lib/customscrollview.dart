d0952dda609a26af964541d661297b92af9df730
hasell-portillo <139635946+hasell-portillo@users.noreply.github.com>
2023-10-22 19:47:38 -0600

    agregando codigos necesarios de singlechildscromllview

--- lib/customscrollview.dart
+++ lib/customscrollview.dart
@@ -1,14 +1,50 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: InicioPage(),
+    );
+  }
+}
+
 class InicioPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Página de Inicio'),
+        title: Text('customscrollview'),
       ),
-      body: Center(
-        child: Text('Contenido de la página de inicio'),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            expandedHeight: 200.0,
+            floating: false,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text('Título'),
+              background: Image.network(
+                'https://via.placeholder.com/350x150',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+                  (BuildContext context, int index) {
+                return ListTile(
+                  title: Text('Elemento $index'),
+                );
+              },
+              childCount: 50,
+            ),
+          ),
+        ],
       ),
     );
   }
