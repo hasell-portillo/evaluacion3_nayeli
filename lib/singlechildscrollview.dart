82e7939a95f3add24ebf20a970775a887c8aef39
hasell-portillo <139635946+hasell-portillo@users.noreply.github.com>
2023-10-22 19:48:11 -0600

    agregando single

--- lib/singlechildscrollview.dart
+++ lib/singlechildscrollview.dart
@@ -1,15 +1,67 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('SingleChildScrollView'),
+        ),
+        body: MySingleChildScrollView(),
+      ),
+    );
+  }
+}
+
+class MySingleChildScrollView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: 200.0,
+            color: Colors.blue,
+          ),
+          Container(
+            height: 200.0,
+            color: Colors.green,
+          ),
+          Container(
+            height: 200.0,
+            color: Colors.orange,
+          ),
+          Container(
+            height: 200.0,
+            color: Colors.red,
+          ),
+          Container(
+            height: 200.0,
+            color: Colors.purple,
+          ),
+          Container(
+            height: 200.0,
+            color: Colors.yellow,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 class AcercaDePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Acerca de'),
+        title: Text('singlechildscrollview'),
       ),
-      body: Center(
-        child: Text('Información sobre la aplicación'),
-      ),
+      body: MySingleChildScrollView(),
     );
   }
 }
