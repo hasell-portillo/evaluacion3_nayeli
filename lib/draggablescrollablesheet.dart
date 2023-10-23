4598a4d0f52c3075059ed5ee9af26a415fe3d5dd
hasell-portillo <139635946+hasell-portillo@users.noreply.github.com>
2023-10-22 19:14:47 -0600

    creando apartado dragge

---
+++ lib/draggablescrollablesheet.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ConfiguracionPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Configuración'),
+      ),
+      body: Center(
+        child: Text('Página de configuración'),
+      ),
+    );
+  }
+}
